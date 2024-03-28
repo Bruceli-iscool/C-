8d1ca468039e52887010548ec431fc317d86dade
Bruce Li <isanoob1897@outlook.com>
2024-03-27 19:52:10 -0700

    add

--- src/compile.py
+++ src/compile.py
@@ -5,42 +5,40 @@ class CodeGenerator:
     def __init__(self, parse_result):
         self.parse_result = parse_result
         self.generated_code = ""
-        self.architecture = platform.architecture()[0]
 
     def generate(self):
-            return self.generate_x64()
+            if platform.processor() == 'i386':
+                return self.generate_x64()
+            elif platform.processor() == 'arm64':
+                return self.generate_arm()
 
 
 
     def generate_arm(self):
         self.generated_code += ".text\n"
         for function_name, statement in self.parse_result:
-            self.generated_code += f".globl {function_name}\n"
-            self.generated_code += f"{function_name}:\n"
+            self.generated_code += f".globl _{function_name}\n"
+            self.generated_code += f"_{function_name}:\n"
             self.generated_code += self.generate_statement_arm(statement)
         return self.generated_code
 
     def generate_x64(self):
         self.generated_code += ".text\n"
         for function_name, statement in self.parse_result:
-            self.generated_code += f".globl {function_name}\n"
-            self.generated_code += f"{function_name}:\n"
+            self.generated_code += f".globl _{function_name}\n"
+            self.generated_code += f"_{function_name}:\n"
             self.generated_code += self.generate_statement_x64(statement)
         return self.generated_code
 
     def generate_statement_arm(self, statement):
         if isinstance(statement, int):
-            return f"    mov r0, #{statement}\n    bx lr\n"
+            return f"    	.cfi_startproc\n; %bb.0:\nmov	w0, #{statement}\nret\n.cfi_endproc"
         else:
             return ""
 
     def generate_statement_x64(self, statement):
         if isinstance(statement, int):
-            return f"    movl, {statement}, %eax\n    ret\n"
+            return f"	pushq	%rbp\nmovq	%rsp, %rbp\nmovl ${statement}, %eax\npopq	%rbp\nretq"
         else:
             return ""
 
-parsed_result = [('main', 42)]
-generator = CodeGenerator(parsed_result)
-generated_code = generator.generate()
-print(generated_code)
